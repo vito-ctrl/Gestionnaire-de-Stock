608f2afcafff4d72f33cff8c46a0b6c25609f412
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2024-12-19 12:40:17 +0100

    addfunction

--- inventory.js
+++ inventory.js
@@ -1,14 +1,42 @@
 const prompt = require('prompt-sync')();
 
 class produit {
-    constructor(name, description, quantite, prix){
+    constructor(id, name, description, quantite, prix){
+        this.id = id;
         this.name = name;
         this.description = description;
         this.quantite = quantite;
         this.prix = prix;
     }
+    great(){
+       console.log(`djfhewf${this.name}`);
+    }
 }
+while(1){
+    console.log("=============== menu ================");
+    console.log("1. Ajouter un produit.");
+    console.log("2. Afficher tous les produits.");
+    console.log("3. Mettre à jour un produit.");
+    console.log("4. Supprimer un produit.");
+    console.log("5. Quitter.");
+    console.log("=====================================");
 
-const pro = new produit("coc", "dhfh gfdehf", 123, "$99.99");
-console.log(`product name is : ${pro.name}`);
+    var chois = prompt("enter you chois : ");
+    switch(chois){
+        case 1:
+            function addproduct(){
+                for(let i = 0; chois == 1; i++){
+                    const id = console.log(`id = ${i + 1}`);
+                    const name = prompt("enter product name : ");
+                    const description = prompt("enter product description : ");
+                    const quantite = prompt("enter product quantite : ");
+                    const prix = prompt("enter product prix : ");
+                    return new produit(id , name, description, quantite, prix);
+                }
+            }
+        case 2:
+            console.log(addproduct());
+        case 5
+    }
 
+}
