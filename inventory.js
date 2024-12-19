aad5b02f870c6dc5403524641169437b71dd79ca
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2024-12-19 17:26:56 +0100

    add listProducts function

--- inventory.js
+++ inventory.js
@@ -13,21 +13,30 @@ class product {
 let products = [];
 
 function addProduct(){
+        console.log("\n======== add product ===========\n");
         const id = products.length + 1;
         const name = prompt("enter product name : ");
         const description = prompt("enter product description : ");
         const quantite = parseInt(prompt("enter product quantite : "));
         const prix = parseFloat(prompt("enter product prix : "));
+        console.log("\n===================================");
         const newProduct = new product(id , name, description, quantite, prix);
         products.push(newProduct);
-        console.log("product added succefuly");
+        console.log("product added succefuly\n");
 }
 function  listProducts(){
     if (products.length == 0){
-        console.log("there is no product");
+        console.log("\nthere is no product\n");
         return;
     }
     console.log("========= all products ========");
+    products.forEach(element => {
+        console.log(`id : ${element.id}`);
+        console.log(`name : ${element.name}`);
+        console.log(`description : ${element.description}`);
+        console.log(`quantite : ${element.quantite}`);
+        console.log(`prix : ${element.prix}`);
+    });
 
 }
 let quit = true; 
@@ -46,7 +55,7 @@ while(quit){
             addProduct();
             break;
         case 2:
-            console.log(addproduct());
+            listProducts();
             break;
         case 5:
             quit = false;
