1b8998790b81af5713d35329f6bf742de4ce4c0b
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2024-12-20 12:03:29 +0100

    add updateProducts function

--- inventory.js
+++ inventory.js
@@ -1,5 +1,7 @@
 const prompt = require('prompt-sync')();
 
+// classe for product 
+
 class product {
     constructor(id, name, description, quantite, prix){
         this.id = id;
@@ -10,8 +12,10 @@ class product {
     }
 }
 
+// array to store products
 let products = [];
 
+// fuction that add products
 function addProduct(){
         console.log("\n======== add product ===========\n");
         const id = products.length + 1;
@@ -24,6 +28,8 @@ function addProduct(){
         products.push(newProduct);
         console.log("product added succefuly\n");
 }
+
+// function that list products
 function  listProducts(){
     if (products.length == 0){
         console.log("\nthere is no product\n");
@@ -39,6 +45,70 @@ function  listProducts(){
     });
 
 }
+
+// function that update products
+function updateProduct(){
+
+    // for user to see products that he can update 
+    listProducts();
+
+    const updateId = prompt("enter the product id : ");
+
+    // find the product that have the same id that user enter
+    const productToUpdate = products.find(index => index.id == updateId);
+
+    // condution for wrong id
+    if(!productToUpdate){
+        console.log(`no product have id ${updateId}`);
+        return;
+    }
+
+    // ask user for updated product info
+    console.log("1 . update quantity");
+    console.log("2 . update prix");
+    console.log("3 . update tow for them");
+
+    let choix = parseInt(prompt("enter your choix : "));
+    switch (choix){
+        case 1:
+            productToUpdate.quantite = parseInt(prompt("enter new quantite : "));
+            break;
+        case 2:
+            productToUpdate.prix = parseFloat(prompt("enter a new prix : "));
+            break;
+        case 3:
+            productToUpdate.quantite = parseInt(prompt("enter new quantite : "));
+            productToUpdate.prix = parseFloat(prompt("enter a new prix : "));
+            break;
+        default:
+            console.log("wrong choix");
+    }
+    console.log("product update succefuly");
+}
+
+// function that delete project
+function deleteProduct(){
+    // 
+    listProducts();
+
+    const deleteId  = parseInt(prompt("enter deleted product id : "));
+
+    const productToDelete = deleteId - 1;
+
+    if(productToDelete === -1){
+        console.log("product not found");
+        return;
+    }
+
+    const sure = prompt("are you sure you want to delete product (yes / no)");
+
+    if(sure.toLowerCase() === 'yes'){
+        
+    }
+
+
+
+}
 let quit = true; 
 while(quit){
     console.log("=============== menu ================");
@@ -49,13 +119,19 @@ while(quit){
     console.log("5. Quitter.");
     console.log("=====================================");
     
-    var chois = parseInt(prompt("enter you chois : "));
+    var chois = parseInt(prompt("enter your choix : "));
     switch(chois){
         case 1:
             addProduct();
             break;
         case 2:
             listProducts();
+            break;
+        case 3:
+            updateProduct();
+            break;
+        case 4:
+            deleteProduct();
             break;
         case 5:
             quit = false;
